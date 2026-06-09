7db324ed69e90a56e67f4c78dd51aa55714eb947
aswin-gopinathan-dev <aswin.gopinathan@outlook.com>
2026-06-09 12:51:41 -0300

    Texture

--- src/main.c
+++ src/main.c
@@ -16,6 +16,8 @@
 #include "../inc/array.h"
 #include "../inc/matrix.h"
 #include "../inc/light.h"
+#include "../inc/triangle.h"
+#include "../inc/texture.h"
  
  
 bool is_running;
@@ -38,8 +40,12 @@ void setup()
     float zfar = 100.0;
     proj_matrix = mat4_make_perspective(fov, aspect, znear, zfar);
 
-    //load_cube_mesh_data();
-    load_obj_file_data("./assets/f22.obj");
+    mesh_texture = (uint32_t*)REDBRICK_TEXTURE;
+    texture_width = 64;
+    texture_height = 64;
+
+    load_cube_mesh_data();
+    //load_obj_file_data("./assets/f22.obj");
 }
  
 
@@ -145,11 +151,16 @@ void update()
         uint32_t triangle_color = light_apply_intensity(face.color, light_intensity_factor);
         
         triangle_t projected_triangle = { 
-                                         {{projected_points[0].x, projected_points[0].y },
+                                         .points = {{projected_points[0].x, projected_points[0].y },
                                          {projected_points[1].x, projected_points[1].y },
                                          {projected_points[2].x, projected_points[2].y }},
-                                         triangle_color,
-                                         avg_depth
+                                         .texcoords = {
+                                             {face.a_uv.u, face.a_uv.v},
+                                             {face.b_uv.u, face.b_uv.v},
+                                             {face.c_uv.u, face.c_uv.v},
+                                         },
+                                         .color = triangle_color,
+                                         .avg_depth = avg_depth
         };
         array_push(triangles_to_render, projected_triangle);
     }
@@ -172,6 +183,7 @@ void update()
 
 void render()
 {
+    draw_grid();
     
     // Iterate the array of projected triangle and draw it on the color buffer
     int num_triangles = array_length(triangles_to_render);
@@ -187,7 +199,8 @@ void render()
                       triangle.color);
         }
 
-        if(render_method == RENDER_WIRE || render_method == RENDER_WIRE_VERTEX || render_method == RENDER_FILL_TRIANGLE_WIRE)
+        if(render_method == RENDER_WIRE || render_method == RENDER_WIRE_VERTEX 
+           || render_method == RENDER_FILL_TRIANGLE_WIRE || render_method == RENDER_TEXTURED_WIRE)
         {
             draw_triangle(triangle.points[0].x, triangle.points[0].y,
                     triangle.points[1].x, triangle.points[1].y,
@@ -200,6 +213,14 @@ void render()
             draw_rect(triangle.points[0].x-3, triangle.points[0].y-3, 6, 6, 0xFFFF0000);
             draw_rect(triangle.points[1].x-3, triangle.points[1].y-3, 6, 6, 0xFFFF0000);
             draw_rect(triangle.points[2].x-3, triangle.points[2].y-3, 6, 6, 0xFFFF0000);
+        }
+
+        if(render_method == RENDER_TEXTURED_WIRE || render_method == RENDER_TEXTURED)
+        {
+            draw_textured_triangle(triangle.points[0].x, triangle.points[0].y, triangle.texcoords[0].u, triangle.texcoords[0].v,
+                    triangle.points[1].x, triangle.points[1].y, triangle.texcoords[1].u, triangle.texcoords[1].v,
+                    triangle.points[2].x, triangle.points[2].y, triangle.texcoords[2].u, triangle.texcoords[2].v,
+                    mesh_texture);
         }
     } 
 
@@ -236,6 +257,12 @@ void process_input()
                 render_method = RENDER_FILL_TRIANGLE;
             if(event.key.keysym.sym == SDLK_4)
                 render_method = RENDER_FILL_TRIANGLE_WIRE;
+       
+            if(event.key.keysym.sym == SDLK_5) 
+                render_method = RENDER_TEXTURED;
+            if(event.key.keysym.sym == SDLK_6)
+                render_method = RENDER_TEXTURED_WIRE;
+            
             if(event.key.keysym.sym == SDLK_c)
                 cull_method = CULL_BACKFACE;
             if(event.key.keysym.sym == SDLK_d)
